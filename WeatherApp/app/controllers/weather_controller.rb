97d3b0a82cc08ea9efa7aa924416b6bf9330ec93
deepti9v <9vdeepti@gmail.com>
2017-07-11 04:09:07 -0700

    Google Places API changes in progress

--- WeatherApp/app/controllers/weather_controller.rb
+++ WeatherApp/app/controllers/weather_controller.rb
@@ -6,6 +6,7 @@ class WeatherController < ApplicationController
   def find
     @result = []
     @forecast = []
+    @nearby = []
     @locations = Location.within_bounding_box(Geocoder::Calculations.bounding_box(params[:location],params[:miles]))
     ForecastIO.configure do |configuration|
         configuration.api_key = '4406bd2ab2d478862f47da3dfb59e1d4'
@@ -14,32 +15,28 @@ class WeatherController < ApplicationController
     @client = GooglePlaces::Client.new('AIzaSyCZe4Xk1xu_4b-ZM-7X8YCe_xHJwRP39yo')
 
     @locations.each do |x|
-      @interests =   @client.spots(x.latitude,x.longitude)
+      @interests =   @client.spots(x.latitude,x.longitude).first(5)
+      @interests.each do |spot|
+          @nearby <<
+           { 'photo' => spot.photos[0].fetch_url(800),
+             'name' => spot.name
+           }
+      end
       @result <<
            {'address' => x.address,
              'latitude' => x.latitude,
              'longitude'=>x.longitude,
              'weather' => ForecastIO.forecast(x.latitude, x.longitude).daily.data.first(5),
-             'interests' => @interests
+             'interests' => @nearby
            }
     end
-
     @job = @result
     gon.watch.job = @job
-
-    puts "****"
-    puts @interests.to_json
-    puts "****"
-
-
-
 
     respond_to do |format|
       #format.html { redirect_to "/weather/index" }
       format.js
     end
-
-
 
   end
 end
